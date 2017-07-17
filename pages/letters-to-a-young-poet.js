e7a7156fb761f75463df4f054c2a2aabf886968c
Thomas O'Brien <tbobrien612@gmail.com>
2017-07-17 08:57:07 -0400

    Add LETTERS TO A YOUNG POET

---
+++ pages/letters-to-a-young-poet.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Post from '../layouts/post'
+import Title from '../components/post/title'
+import P from '../components/post/paragraph'
+import Quote from '../components/post/quote'
+import HR from '../components/post/hr'
+
+export default () => (
+  <Post>
+    <Head><title>LETTERS TO A YOUNG POET</title></Head>
+    <Title>LETTERS TO A YOUNG POET</Title>
+    <P>A few notes on some fine art called <a href="https://kbachuntitled.files.wordpress.com/2013/04/rainer-maria-rilke-letters-to-a-young-poet.pdf">Letters to a Young Poet</a> by Rilke.</P>
+    <Quote>"I cannot discuss your verses; for any attempt at criticism would be foreign to me. Nothing touches a work of art so little as words of criticism: they always result in more or less fortunate misunderstandings."</Quote>
+    <P>SAME. I cannot discuss your verses either. I think it's why I often labor to call people out for most things, even when it’d be appropriate. I procrastinate on judgment, assuming I prob don’t understand many things.</P>
+    <P>What to write about?</P>
+    <Quote>"Don’t write love poems; avoid those forms that are too facile and ordinary: they are the hardest to work with, and it takes great, fully ripened power to create something individual where good, even glorious, traditions exist in abundance… rescue yourself from these general themes and write about what your everyday life offers you; describe your sorrows and desires, the thoughts that pass through your mind and your belief in some kind of beauty — describe all these with heartfelt, silent, humble sincerity and, when you express yourself, use the things around you, the images from your dreams, and the objects that you remember."</Quote>
+    <Quote>"If your everyday life seems poor, don’t blame it; blame yourself; admit to yourself that you are not enough of a poet to call forth its riches."</Quote>
+    <Quote>“...wouldn’t you still have your childhood, that jewel beyond all price, that treasure house of memories? Turn your attentions to it."</Quote>
+    <P>Ugh, that jewel. I need to journal more, always.</P>
+    <Quote>"A work of art is good if it has arisen out of necessity. That is the only way one can judge it.”</Quote>
+    <P>You could replace “work of art” with a lot of things.</P>
+    <P>That first letter was fire. Read it a couple times.</P>
+    <P>Have you seen a recommendation go harder than this though?</P>
+    <Quote>"Now Niels Lyhne will open to you, a book of splendors and depths; the more often one reads it, the more everything seems to be contained within it, from life’s most imperceptible fragrances to the full, enormous taste of its heaviest fruits. In it there is nothing that does not seem to have been understood, held lived, and known in memory’s wavering echo; no experience has been too unimportant, and the smallest event unfolds like a fate, and fate itself is like a wonderful, wide fabric in which every thread is guided by an infinitely tender hand and laid alongside another thread and is held and supported by a hundred others. You will experience the great happiness of reading this book for the first time, and will move through its numberless surprises as if you were in a new dream. But I can tell you that even later on one moves through these books, again and again, with the same astonishment and that they lose none of their wonderful power and relinquish none of the overwhelming enchantment that they had the first time one read them."</Quote>
+    <a href="https://kbachuntitled.files.wordpress.com/2013/04/rainer-maria-rilke-letters-to-a-young-poet.pdf">Full Ting</a>
+  </Post>
+)

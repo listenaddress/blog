a79d7250fa46b0fa4d5a27844e3fd2e95eb56658
Thomas O'Brien <tbobrien612@gmail.com>
2017-07-17 13:44:38 -0400

    Update url

--- layouts/main.js
+++ layouts/main.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default ({ children }) => (
   <div className="main">
     <div className="logo">
-     <Link href="/"><a>youngdayjob.com</a></Link>
+     <Link href="/"><a>youngdayjob.now.sh</a></Link>
      {' '}
      (<a href={`https://github.com/thomasbeta/blog`} target="_blank">src</a>)
     </div>
